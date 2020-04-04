195b317871590b8dc236b68ed3036c80d0ee5492
xyten <17376515259@163.com>
2020-04-04 10:18:12 +0800

    edit01

--- main.c
+++ main.c
@@ -1,214 +1,82 @@
-#include <fcntl.h> 
-#include <unistd.h>
-#include <stdlib.h>
-
-/*
-
-Huawei elite challenge
-
-date:   20200404
-version:1.0
-by:     xyTen
- */
-
-
-typedef char bool; 
-#define TRUE        1
-#define FALSE       0
-
-#define IDSIZE      12
-
-
-#define STDOUT      1
-#define STDERR      2
-#define MAXBUF      10000000
-#define MAXSETVEC   1000000
-#define MAXPRIM     999943
-
-
-char *datafile = "/home/jialu/Desktop/Huawei/test_data.txt";
-char *ansfile  = "00";
-
-
-//set struct
-typedef struct circlenode{
-    char id[IDSIZE];
-    struct circlenode *next;
-}CircleNode, *pCircleNode;
-
-typedef struct hashnode{
-    char id[IDSIZE];
-    pCircleNode circle;
-    struct hashnode *next;
-}HashNode, *pHashNode; 
-
-typedef struct hash{
-    pHashNode set_vec[MAXBUF];
-}Set;
-
-
-//set function
-void set_init(Set *set);
-pHashNode set_add(Set *set, const char *id);
-bool set_count(const Set *set, const char *id);
-pHashNode set_find(const Set *set, const char *id);
-
-
-//kernel function
-bool load_data_to_set(Set *set, char *buf);
-
-
-// general function
-bool str_equ(const char *s1, const char *s2);
-void str_cpy(char *s, const char *t);
-
-int main(){
-
-    //set init
-    Set *set = (Set*)malloc(sizeof(Set));
-    set_init(set);
-
-    //data read
-    int fd;
-    fd = open(datafile, O_RDONLY, 0666);
-    char *buf = (char*)malloc(MAXBUF);
-    unsigned cnt = read(fd, buf, MAXBUF);
-    buf[cnt]='\0';
-
-    //load data to set 
-    if(load_data_to_set(set, buf)==FALSE){
-        write(STDERR, "load data to set error...", 20);
-        exit(1);
-    }
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-    write(STDOUT, buf, 50);
-
-
-    return 0;
-}
-
-
-
-
-// all function
-
-void set_init(Set *set){
-    for(int i=0; i<MAXSETVEC; ++i)
-        set->set_vec[i] = NULL;
-}
-
-pHashNode set_add(Set *set, const char *id){
-    int sum=0;
-    while(*id!='\0')
-        sum += *id++;
-    sum %= MAXPRIM;
-    pHashNode p = set->set_vec[sum];
-    if(p==NULL){
-        p = (pHashNode)malloc(sizeof(HashNode));
-        p->circle = NULL;
-        p->next = NULL;
-        set->set_vec[sum] = p;
-    }
-    while(p->next){
-        p = p->next;
-        if(str_equ(p->id, id)==TRUE)
-            return p;
-    }
-    pHashNode q = (pHashNode)malloc(sizeof(HashNode));
-    str_cpy(q->id, id);
-    q->next = NULL;
-    q->circle = NULL;
-    p->next = q;
-    return q;
-}
-
-bool set_count(const Set *set, const char *id){
-    int sum=0;
-    while(*id!='\0')
-        sum += *id++;
-    pHashNode p = set->set_vec[sum%MAXPRIM]->next;
-    while(p && str_equ(p->id,id)==FALSE)
-        p = p->next;
-    return p==NULL?FALSE:TRUE;  
-}
-
-pHashNode set_find(const Set *set, const char *id){
-    int sum=0;
-    while(*id!='\0')
-        sum += *id++;
-    pHashNode p = set->set_vec[sum%MAXPRIM]->next;
-    while(p && str_equ(p->id,id)==FALSE)
-        p = p->next;
-    return p;
-}
-
-bool load_data_to_set(Set *set, char *buf){
-    char id1[IDSIZE], id2[IDSIZE];
-    char *iid1=id1, *iid2=id2, *buff=buf;
-    while(*buff!='\0'){
-        iid1=id1;
-        iid2=id2;
-        while(*buff!=',')
-            *iid1++ = *buff++;
-        *iid1 = '\0';
-        buff++;
-        while(*buff!=',')
-            *iid2++ = *buff++;
-        *iid2 = '\0';
-        while(*buff!='\n')
-            buff++;
-        buff++;
-        pHashNode p = set_add(set, id1);
-        pHashNode q = set_add(set, id2);
-        pCircleNode s = p->circle;
-        if(s==NULL){
-            s = (pCircleNode)malloc(sizeof(CircleNode));
-            s->next = NULL;
-            p->circle = s;
-        }
-        while(s->next)
-            s = s->next;
-        pCircleNode t = (pCircleNode)malloc(sizeof(CircleNode));
-        str_cpy(t->id, q->id);
-        t->next = NULL;
-        s->next = t;
-    }
-
-
-    return TRUE;
-}
-
-
-
-
-
-
-
-
-void str_cpy(char *s, const char *t){
-    while(*t!='\0') *s++ = *t++;
-    *s = '\0';
-}
-
-
-bool str_equ(const char *s1, const char *s2){
-    while(*s1!='\0' && *s2!='\0' && *s1++ == *s2++)
-        ;
-    return (*s1==*s2=='\0');
-}
+U2FsdGVkX19kgOr+MCYmCZw/pbV+6mazMLWMCG+yeRrGiJBosUJ2dC8L8vySvOtg
+tyUw7NrBJ4GwvwTH5jwja5wuvIGJv4bjpEtprwBjP/zJzKMQsi3yU2lzfqPGT47H
+BFVahcJRmeUFVwFVwCtBcd9CzhHt44pb+ewDvVcTa/kRyo299hPY0MkrCrpwVFcp
+rCaATBoRRh7o7ZU9eOqzJjZvcybTDQ1hWd7RegMXuoMTVwQ2+MfF5lrATTN+4Fa0
+LCvClxN4UzJniewYGUVwZohLup5kM7e6YvOf3xIWbAODDxyh1YT1qBW3fELUrl77
+3h5v5EY2m4nlJxdsoeOf+GFW0FSeMyA2owobiRffHo1YIJkf9iwnHtxnHebjyJD/
+eyh9SDRqzGqzYFqUFzQidTNNc+HuB18IyonfgxpUQ2gfcHEUHQ0xqx9hMaghRQ/n
+lfs+9o/nSGLUyls2K9VIaIbPsgnlSBCi77zCKaLkAOvqkws0R9ahO665wW0n9g9V
+LY9xTURX0vt7FxjTqxytutAPwyMlZYt6jWBrpL2HWH+vq+Fo4n5wJDLuupTI5hJh
+PGWWswmVEbxYVvAdqCX8clwdTESV/RfiPUqjy1YIDi1wUDpN0yrtWO8VLWijYucZ
+1gKghECpFa5jNr+ee88hwohTtAAT66fO3EFMp+WC06Mzu1QkgW6ufk1hn5AWafy0
+fjn3/O+H+TbboJv3wOT3vGMFhR08QX3VO2ekpqKkF2WyP5kIGDjVEbAaUV2H2Ahh
+aaS5Tu0PBjC/oHTBUZzsbxg4ugGwu5b3o1M58qUSvsxvYUYLahREM3kyUIiL5UH8
+dGlteFE/4qBS6N9pgRYcHwm1kMRIO5xC4OjnJiwGoUt9ndnZ1pD1GvXOh6TXRa1T
+2pxZVZwPOU2iJrkqEnXudgR7JDc27JVqcwmy8EtmALMZgOii98R8NXBBcftJWqoM
+byvHEWX795FDDjqrK8AiTjFwmA+gzC3dw2m6ZiA15hR+ELeYaZSvHDLEstO3jBTJ
+MxVSDp8XBO5i0MLfnuUzzydR2iA1/0bCgkEAuJ/JPeUyyh1O9VcKVgfYf2h9E2dT
+mojsn2kKpSi9P558JmwT0I+62D36ACh6v0dAiApofcDGyH+YGA5BrxXxGXtYtrqw
+X4WUC/h19tuP0GrCx2krIzSKWxAuKs3gyHa5H/w/TA0EN1Xk1yBJ8fvs4zlNq25/
+6bYJpDIHT1iHEOnvKozgYjav2OF2vWG3ONTgcnrlyH9+s9BXBFh+iGp7gggOlN/3
+sEvbKAz0wmmB6OdvVnIvAEuz+WAytqKk4JHfvD2n0+gsdEZOHn2Olt5xiDl2YAzn
+ivQT4Xc6s1IkZVTR+IkMxvx+7JlCQe3EQI9yrJv/P5DMDNHIRz+b2mOgonfSmunK
++5kRaLLa9H6USZ5GXWsBk/9CzuaXjRb9S6VVCZGD0dPoBW9JaH0SaitEe3NmGrx9
+Cmv7enzvhOD0GyBYSRwTUUvQw0m82F8PK+ehzRaCKZq8aoy4BelfDEiBQ8NlSPzI
+m5wgw0qTFyULIHZ3Y4BeULUM6MorVjvr+T7JtPlL2xwoHrKVhFT/hSkiZhJCgGUK
+mbcxmEWRyUYM9obyZNVOHEhW+99BfOhaIkXcCiBWpHF6w52QJwPU/Xp5Qr/Hm2Rt
+VWlRB+NqPTVWqnFF8qytMVM0+xxUXQIGOMhCtN8ISrPLkUX6yUWlrkKUyrpCy1tc
+ihUyShVp/kOaM3MIZQETNQ7zAyC0bfi04yxSVYYOuf13sOI0tmBaBDQOAFLjWO68
+Bar77i+YEbXApN1taMLc94cSboaBHJnxeR2yaSYma0vJ35BP68nS8ivMV5nJVt+H
+lmqAWNjbULdCoErtZYHyAcpl7mAaaQOU6eHSBgCmqjy46+S9qZVloi68KhMvAF1x
+BLfli4s5lNa1mDGr9AP/vQctzJEopMtk5l3PLhxosjgb9w3TpIXt3PiB+o/RLpr9
+G7ioZBsdvwbyhYgoX09R4ok+srf3cr6y1WGnLd0WQ/o6wiXdqNnBTJqK9+0C9Ztv
+tlCfej/tCyGy4fLjfeeUO/lxzH9WUTdEaCRsnmngH99OqeNfY+3AvdjtC0PP8mee
+rw8ogtZDdrV5h7oqG4UXQm2xUTofGOeWgkFt0GCM1KLgiwlgxn6n3DbSlIofwamS
+zDme4Yfqti8lT5cTLyo9ZDQUOwR9csDjmtQiNgvqUwUZeljbrRxQXRbXfonhi5lZ
+VIRVWDXq6Ip7+t37htQVCH0hLdNMr4OsdDln1pXs7oPPINgVg0CCtgQEq4CjxdhG
+Txh1M66DY7ahglYTBmR3vEzl6IbSj8NnzJpOMVBOmzdV4TIXOnOrtzszdpkPVkCY
+b+sk+Bw88GgxsxI7SugyBy9Lkfmh1aMQ/mR0Wp9P9gtL0o/xk91m8lUuGnKVk99Y
+kwEvSsOs3OFU0ViDq7z++VlaGNkkHCZ3+cAEGJSGlX6ASTzy+BbkNJdsdhJguRxz
+qQEHE6JKtBx1+MMYAYmmSQOI6f/lPof3uf9qZ1N7Kvz5ewkwLry7D6CQY547Kg7N
+JAtQq+57WAL4HeTtL4b4nQT4XZnB17NzxkMKnkAYUH9bnaYDIthyjMNSC1nb12uM
+nrY9Zssfoke6oCr4VdfFwcZGeph/D3OINBVokVJnemhw6IvYr3sFmry/96X2yMBi
+ueSQrBAmefWt9ckHqecGv9rMCVgDwnamCBO3M8OFGY3Byi8CRO9RtsTbIiftbmbt
+3K0GCaE/GnpGkpKzHUTBxNIW8CzVPu+/JxyjIg8VVXfc/o92n6PekEE2oxOPDVEh
+2V3CLoISyZdfHgDhLnsF2IuEzZHnQ2BiR4KhI3oBD5v8Wr3HHhdADSqAL+SqZF5U
+yx3pkYZ41jwHG96ANo7raKXlyK5XJb6t0wmQIOd2H5VB02nEVDZ6ZHl1tjrp0mPg
+R90modbD/w/TdFi8lO6K1RzuBR+6eKx+yM5GFVrSKvlIchWJCB12CzyzYC3YBM3Z
+yBW7/17xutj5xLp7z/2gZCEK4gM9HHNCrrJ5NGGuC3406z3X/6F9i5G8GK6pr6Rp
+b7QjsZsj4l6AK5Zh7x+iVSmXlVX6R4YLEGB/8UpQCmD6KD2r3t1tJk4s4k5hXk+q
+jQz1zOTTaTSFnllYoRxdMkm8yjP0DDTGdJkHfZWu1czKqdtUbjS0YpSQlRMyQuNV
+Pwp6vTsjX6QyxrcqkFwEQxh18HaGom838HpW0zPr9FzNWgt0JZiBJwpVCV9IGDSr
+pBQyw++KpWJc9GI1RlhXagvwn1fyh7L2FztOtAlYfXBZYFIM9bWasFHF9kdepzXR
+DR5YewlctMgt6NsDekPCEymYvI7oWhPhoRyyYAnf+hrlDsPnWwKtO6Vm5orKH6lr
+0EcEmD0GUThlMSskfeLx2RSjufHfPuqv/C7Iy1C9qJvFUuUERlM5C5L7sKLqV7/g
+y51dAFxaBRMcZQbDWYkYC6/XYMvPTuicgRbR9ILCc5BOa69RBDZKtO8mXNT71TsF
+Um1rr8Zy4tjknkyNaBSayW2kz35fkH6N1k0P5eOOwb3Jz7vPbDKNH5E4CGU/jO92
+ff1+1/WukC8l6b6+xOSj9ODkDerW5EzeB/Fzu98YJ/EaOsw8Ib+e82A0DKYkEp/q
+cqxF02+JU2wgkBrA/m3/1n3pCVuAvaB/GTMpNKMjwOD3k2qLy+WIMyXVymg/g+w4
+S8VuuiGe5Dp8zJMF3WhdpKYSBrMUva/wou1595GTSHrNyzadWKZeg1faeGOLGmSP
+sJ6+wKUgawS3mAc8lF2JxT5oYmGB04GQE29WwW+h3x/L/Tb69r92Lp86UI+UmKU5
+zDKFHZCAg1h6+Y4TcsaoiYZc+p8NVXT2+ItrqWc1YyU3VjNlKvjCzNNakyM9ZHdB
+iWnMAZIsLvbSfKDe9i4vGx6+ZbzHR/qaIu1dtcgzz6SWcY/BdldepMB/nRSKbPad
+47Pa+WsOtmN+E275wH2mMwe48pFip1oHKfahAPZy6EEwp7dieOAUVQraP1HCi5Fk
+Ps7jcvQCMmysEVsXCJd2mYCV9jRGbeyhrR5J5buusHCSuAD9Yn2pANWncCRHjfQa
+wDqIsBWX3udynvdswoWfgC4IeiniolDQkX55EAZzKrUBvQb8XwuQ2YFo2UIAmjTt
+36I8uaK/sxCgOM6sbnpapF88PVauxGRIs7aJyUv/DLsVIY93yrE7Zc/DtDT7Lw5d
+tQ7Pqi40vl5JJ1bm9caQyN2RNmt0ciMkMs7lQgDot4TuKeUOVlwpHcuKL56SG7E6
+iJaHKnlpG834wIduTL/pKqahCjTuRTShyP2raH5YnYX+auWFIBUfplCu8CXagtWh
+IQOxj6TSPIbTzsFBUGqrb5E1UZSdnFs+0OnEwtdLNrMRXz56jXjqnP/OB/wWsb40
+lTSn71G5HVKqbjcuf6u1G25UnLJyDH6JnEr82+BRD8U5wdQEcr0xvNunLTWzF50f
+iloFnyGXeoO8YDI12QlpMLDydkaGeB+m1VRSL+IKcOdueZie9qxEoKeeHyWqGtO1
+pje1CrdnZ/Tc8f33yKK70YxShIW0V3T0MR/+epJmScOrokW06aerR8TFePE5GkNY
+1rjHuvSOzp5ER66d8Xj32E/PCa4AwVHxsXe6oWkbno13c9dOyY5ZxoAG3JdRiFgf
+ZbpM1D9OSjqNAs3OaPtqKbEl4C9I/J2hjEoR2BbCVB6DtD/ewm1de1DtF29q5h1c
+wfofDFw7yJKjVqscjRWznMua3fLm/QIlxiOofYDDj7uAjwmCG7LqYG4M5Bs8P9vE
+NLVAFmsrXYLZsLYdXUAoBZrgAz5Cd4OXiDgwIjTXv/B/aiCUGCNJvS+/jXVhKEnc
+EkCVKLSJ1uvLa4EI49c1538mgbfONJVzeUTfdbwYkOUrUw5Q3uQX4235Qg7V+hw0
+TMol19Kq+nbXXgUiMOdtWSRHevVPuM9grpiBoF3TI9gZu1VilD5dFNCCz3W/hnal
+J9AilrXyHj64GSScIqUtJFXcdWiOgUNeyKIoMXkm7SrwLwxqtdBf0IOa0GxQqVn7
+G5HrEm6iWVYduqn3Fq4zrzqE3vaERE+jkQAdwPEyFiK3I9eT6FmAB8GUJykUGkKY
+x9JTFbxZKkS8I+iiQv35ZwYi7z7k/UX58tLb785D54ay6cP3pGG6A23MYMWEqfWM
+5ocJAyYd7kNBNRgVu+3E/PGF9BYr3kuHTjMDkbagu8A=
